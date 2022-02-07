076c61d0bc5458eb914b9bb400c81244edc27d51
Matthew <matthewrr@users.noreply.github.com>
2022-02-06 16:09:29 -0800

    Add exception handling for both invalid URLs and failed data extractions

--- simplifyRecipe/views.py
+++ simplifyRecipe/views.py
@@ -2,31 +2,33 @@ from django.shortcuts import render
 
 import re, requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 ingredient_header = ['ingredients', 'ingredients:']
 instructions_header = ['instructions', 'instructions:', 'steps', 'steps:', 'directions', 'directions:']
-testing_urls = ['https://damndelicious.net/2014/12/13/pasta-sun-dried-tomato-cream-sauce/',
-                'https://www.recipetineats.com/chicken-with-creamy-sun-dried-tomato-sauce/',
-                'https://www.livewellbakeoften.com/soft-and-chewy-chocolate-chip-cookies-recipe/',
-]
 
-def get_text(soup):
+def get_text(soup, url):
+    source = urlparse(url).hostname
     title = soup.find('h1').text
     for header in soup.find_all(re.compile('^h[1-6]$')):
-        header_text = (header.text).lower()
+        header_text = header.text.lower()
         if header_text in ingredient_header:
-            # ingredient =  re.compile('[A-Za-z0-9 _.,!"'/$]*')
-            # re.sub(r'[^\x00-\x7f]',r'', ingredient.text)
             ingredients = [re.sub(r'[^\x00-\x7f]',r'', ingredient.text) for ingredient in header.parent.findAll('li')]
         elif header_text in instructions_header:
             instructions = [instruction.text for instruction in header.parent.findAll('li')]
-    return {'ingredients': ingredients, 'instructions': instructions, 'title': title}
-    
+    return {'ingredients': ingredients, 'instructions': instructions, 'source': source, 'title': title, 'url': url, 'valid_url': True, 'extraction': True}
+
 def simplify_recipe(request):
-    recipe_data = {}
+    recipe_data = {'valid_url': False, 'extraction': False}
     if request.method == 'POST':
         url = request.POST['url']
-        reqs = requests.get(url)
-        soup = BeautifulSoup(reqs.text, 'html.parser') 
-        recipe_data = get_text(soup)
+        try:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser') 
+            recipe_data = get_text(soup, url)
+        except UnboundLocalError as err:
+            recipe_data['valid_url'] = True
+            print('UnboundLocalError. Could not extract data from valid URL.')
+        except requests.exceptions.MissingSchema as err:
+            print('Missing Schema')
     return render(request, 'simplifyRecipe/recipe_temp.html', recipe_data)
