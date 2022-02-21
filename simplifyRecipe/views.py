979c4fb8bdd08a30d738206eb7d1705e4f33144b
Matthew <matthewrr@users.noreply.github.com>
2022-02-20 16:19:17 -0800

    Expand recipe extraction to include additional DOMs

--- simplifyRecipe/views.py
+++ simplifyRecipe/views.py
@@ -7,15 +7,55 @@ from urllib.parse import urlparse
 ingredient_header = ['ingredients', 'ingredients:']
 instructions_header = ['instructions', 'instructions:', 'steps', 'steps:', 'directions', 'directions:']
 
-def get_text(soup, url):
+def extract_data(header):
+    pattern = re.compile(r'[^\x11-\x7f\u00BC-\u00BE]') #standard ascii minus first 11 (10 is newline) plus fractions
+    unnested = ['p']
+    nested_single = {'tr':'td'}
+    nested_multiple = {'ul':'li','ol':'li','tr':'td'}
+    result = []
+
+    for tag in unnested:
+        items = header.findAll(tag)
+        for item in items:
+            result.append(re.sub(pattern, r'', item.text))
+        if result: return result
+    for k, v in nested_single.items():
+        parent_items = header.findAll(k) 
+        for item in parent_items:
+            child_items = item.findAll(v) #combine with finding text
+            l = [child.text for child in child_items]
+            result.append(' '.join(l))
+        if len(result) > 1:
+            return result
+        else:
+            result.clear()
+    for k, v in nested_multiple.items():
+        parent_items = header.findAll(k)
+        for item in parent_items:
+            child_items = item.findAll(v)
+            for child_item in child_items:
+                find_text = child_item.findAll(text=True)
+                temp = []
+                for item in find_text:
+                    stripped = item.strip()
+                    if stripped:
+                        temp.append(stripped)
+                ' '.join(temp)
+                result.append(' '.join(temp))
+        if result: return result
+    return result
+
+def get_recipe(soup, url):
     source = urlparse(url).hostname
     title = soup.find('h1').text
     for header in soup.find_all(re.compile('^h[1-6]$')):
-        header_text = header.text.lower()
+        header_text = (header.text.lower()).strip()
         if header_text in ingredient_header:
-            ingredients = [re.sub(r'[^\x00-\x7f]',r'', ingredient.text) for ingredient in header.parent.findAll('li')]
+            ingredients = extract_data(header.parent)
+            if not ingredients: ingredients = extract_data(header.parent.parent)
         elif header_text in instructions_header:
-            instructions = [instruction.text for instruction in header.parent.findAll('li')]
+            instructions = extract_data(header.parent)
+            if not instructions: extract_data(header.parent.parent)
     return {'ingredients': ingredients, 'instructions': instructions, 'source': source, 'title': title, 'url': url, 'valid_url': True, 'extraction': True}
 
 def simplify_recipe(request):
@@ -26,10 +66,14 @@ def simplify_recipe(request):
         try:
             r = requests.get(url)
             soup = BeautifulSoup(r.text, 'html.parser') 
-            recipe_data = get_text(soup, url)
+            recipe_data = get_recipe(soup, url)
         except UnboundLocalError as err:
             recipe_data['valid_url'] = True
             recipe_data['modal'] = 'failed_extraction'
         except requests.exceptions.MissingSchema as err:
             recipe_data['modal'] = 'invalid_url'
-    return render(request, 'simplifyRecipe/recipe_temp.html', recipe_data)
+    return render(request, 'simplifyRecipe/recipe_card.html', recipe_data)
+
+#list sites that require sign-in
+#error out if no bullets
+#https://recipeland.com/recipe/v/bhg-fudge-brownies-50409 table, paragraphs, metric issue
